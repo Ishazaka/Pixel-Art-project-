499479847cccab8bc4b09ccffac0c5567da9db4e
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-01-26 12:39:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ const sizeEl = document.querySelector('.size')
 let size = sizeEl.value
 const color = document.querySelector('.color')
 const resetBtn = document.querySelector('.btn')
+
+let draw = false
+
+function populate(size) {
+  container.style.setProperty('--size', size)
+  for (let i = 0; i < size * size; i++) {

bd86fc496cd47740ddf4a26fefecb6f4f384972f
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-01-26 12:38:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+//  -----   ---pixel art ----
+
+
+
+const container = document.querySelector('.container')
+const sizeEl = document.querySelector('.size')
+let size = sizeEl.value
+const color = document.querySelector('.color')
+const resetBtn = document.querySelector('.btn')
